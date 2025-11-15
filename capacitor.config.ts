0f01dd547265557363e97d85261e59ab53f9c52b
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-11-15 08:28:31 -0600

    4

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.salesapp.app',
-  appName: 'sales',
+  appName: 'Sales',
   webDir: 'www'
 };
 
